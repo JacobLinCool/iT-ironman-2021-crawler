1229c38ae207b300d312d8bdd0083a3a8bc29ac5
JacobLinCool <jacoblincool@gmail.com>
2021-09-25 23:04:31 +0800

    Update crawler.js

--- script/crawler.js
+++ script/crawler.js
@@ -6,45 +6,96 @@ const { JSDOM } = require("jsdom");
 
 process.env.TZ = "Asia/Taipei";
 
+let DownloadT = 0,
+    ParseT = 0,
+    OrganizeT = 0,
+    SaveT = 0;
+
+let available = 12;
+const finished = [];
+function isAvailable() {
+    return new Promise((resolve) => {
+        if (available > 0) {
+            available--;
+            resolve();
+        } else finished.push(resolve);
+    });
+}
+
 main();
 
 async function main() {
     const START_TIME = new Date();
+
     const baseURL = "https://ithelp.ithome.com.tw/2021ironman/";
     const types = ["contest", "self"];
 
     const results = await Promise.all(types.map((type) => crawler(baseURL + type)));
+    const OT = Date.now();
     const data = results.reduce((a, b) => a.concat(b), []);
     data.sort((a, b) => b.view - a.view || b.date[1] - a.date[1] || b.date[2] - a.date[2]);
+    OrganizeT += Date.now() - OT;
 
+    const ST = Date.now();
     saveData(data);
+    SaveT += Date.now() - ST;
 
     const END_TIME = new Date();
     console.log(`Crawled ${data.length} Articles in ${((END_TIME - START_TIME) / 1000).toFixed(2)}s`);
+    console.log(
+        `Download: ${(DownloadT / 1000).toFixed(3)}s, Parse: ${(ParseT / 1000).toFixed(3)}s, Organize: ${(OrganizeT / 1000).toFixed(3)}s, Save: ${(
+            SaveT / 1000
+        ).toFixed(3)}s`
+    );
 }
 
 async function crawler(startURL) {
     const result = new Map();
 
-    let nextURL = startURL;
-    let page = 1;
-    while (nextURL) {
-        console.log(`Crawling Page ${page}`);
-        const html = await fetch(nextURL).then((res) => res.text());
-        const dom = new JSDOM(html);
-        const document = dom.window.document;
-
-        const articles = document.querySelectorAll("li.ir-list");
-        for (const article of articles) {
-            const parsed = parseArticle(article);
-            result.set(parsed.link, parsed);
-        }
-
-        nextURL = document.querySelector(".pagination > .active")?.nextElementSibling?.querySelector("a")?.href;
-        page++;
+    const firstPage = await task(startURL);
+    firstPage.result.forEach((val, key) => result.set(key, val));
+    let total = firstPage.pageCount;
+    const tasks = [];
+    for (let i = 2; i <= total; i++) {
+        await isAvailable();
+        tasks.push(
+            task(startURL + "?page=" + i).then((page) => {
+                page.result.forEach((val, key) => result.set(key, val));
+                if (page.pageCount > total) total = page.pageCount;
+            })
+        );
+        if (i === total) await Promise.all(tasks);
     }
 
     return [...result.values()];
+}
+
+async function task(url) {
+    console.log(`Crawling Page: ${url}`);
+    const result = new Map();
+    const DT = Date.now();
+    const html = await fetch(url).then((res) => res.text());
+    DownloadT += Date.now() - DT;
+
+    const PT = Date.now();
+    const dom = new JSDOM(html);
+    const document = dom.window.document;
+
+    const pageCount = parseInt(document.querySelector(".pagination > li:nth-last-child(2)").textContent);
+
+    const articles = document.querySelectorAll("li.ir-list");
+    for (const article of articles) {
+        const parsed = parseArticle(article);
+        result.set(parsed.link, parsed);
+    }
+    ParseT += Date.now() - PT;
+
+    if (finished.length > 0) {
+        const resolve = finished.shift();
+        resolve();
+    }
+
+    return { result, pageCount };
 }
 
 function parseArticle(article) {
